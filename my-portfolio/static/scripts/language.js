5778febc8993788731e7ca41ebfe0ce0b9ae7e85
Anton Morozov <anomorch@gmail.com>
2022-09-29 10:48:16 +0300

    multilanguage of projects

--- my-portfolio/static/scripts/language.js
+++ my-portfolio/static/scripts/language.js
@@ -21,6 +21,19 @@ const data = {
             'p2': 'My target is to deliver to society your idea which ' +
                 'will promote your business and make your work recognizable.',
             'img': 'My Photo'
+        },
+        'projects': {
+            'header': 'Projects',
+            'subHeader': 'These are the open projects I am proud about.',
+            'simetraP1': 'Rating of Cities',
+            'simetraP2': 'The rating helps to collect statistics about ' + 
+                'quality of public transport in Russian cities.',
+            'votingSystemP1': 'Voting System',
+            'votingSystemP2': 'The system is desgned to give community ' + 
+                'a tool to create a democratic election.',
+            'subPSHeader': "Hey... Pss... There are even more projects " + 
+                "created by me. However, they are closed due NDA. But " + 
+                "I haven't told it you, bro ;)." 
         }
     },
     'russian': {
@@ -36,11 +49,25 @@ const data = {
         'about-me': {
             'name': 'Антон Морозов',
             'p1': 'Начинающий увлеченный российский full-stack ' +
-                'разработчик, готовый помочь вам реализовать проект, ' +
-                'который покажет всему миру ваши настоящие ценности.',
+            'разработчик, готовый помочь вам реализовать проект, ' +
+            'который покажет всему миру ваши настоящие ценности.',
             'p2': 'Моя цель - это продвижение в общество вашей идеи, ' +
-                'которая продвинет ваше дело и сделает его узнаваемым.',
+            'которая продвинет ваше дело и сделает его узнаваемым.',
             'img': 'Моя Фотография'
+        },
+        'projects': {
+            'header': 'Проекты',
+            'subHeader': 'Это открытые проекты, которыми я горжусь.',
+            'simetraP1': 'Рейтинг Городов',
+            'simetraP2': 'Данный рейтинг помогает собрать статистику о ' +
+                'качестве общественного транспорта российских городов.',
+            'votingSystemP1': 'Избирательная Система',
+            'votingSystemP2': 'Данная система разработана, чтобы дать ' +
+                'сообществу инструмент для проведения демократических ' + 
+                'выборов.',
+            'subPSHeader': 'Хей... Псс... У меня есть еще больше проектов, ' +
+                'разработанных мной. Однако, они закрыты по NDA. Но я тебе ' +
+                'этого не говорил, мужик ;).' 
         }
     }
 }
@@ -93,11 +120,40 @@ function changeAboutMeLanguage(currentLanguage) {
     p2.textContent = data[currentLanguage]['about-me']['p2'];
 }
 
+function changeProjectsLanguage(currentLanguage) {
+    const projects = document.getElementsByClassName('projects')[0];
+
+    const header = projects.querySelector('h1');
+    const subHeader = projects.querySelector('h2');
+
+    const simetra = projects.querySelector("div[name='simetra-rating']");
+    const simetraP1 = simetra.querySelectorAll('p')[0];
+    const simetraP2 = simetra.querySelectorAll('p')[1];
+
+    const votingSystem = projects.querySelector("div[name='voting-system']");
+    const votingSystemP1 = votingSystem.querySelectorAll('p')[0];
+    const votingSystemP2 = votingSystem.querySelectorAll('p')[1];
+
+    const subPSHeader = projects.getElementsByClassName('ps-note')[0];
+
+    header.textContent = data[currentLanguage]['projects']['header'];
+    subHeader.textContent = data[currentLanguage]['projects']['subHeader'];
+
+    simetraP1.textContent = data[currentLanguage]['projects']['simetraP1'];
+    simetraP2.textContent = data[currentLanguage]['projects']['simetraP2'];
+
+    votingSystemP1.textContent = data[currentLanguage]['projects']['votingSystemP1'];
+    votingSystemP2.textContent = data[currentLanguage]['projects']['votingSystemP2'];
+
+    subPSHeader.textContent = data[currentLanguage]['projects']['subPSHeader'];
+}
+
 languagesLink.forEach(newCurrentLanaguge => {
     newCurrentLanaguge.addEventListener('click', () => {
         changeCurrentLanguage(newCurrentLanaguge);
         const currentLanguage = newCurrentLanaguge.getAttribute('language');
         changeNavbarLanguage(currentLanguage);
         changeAboutMeLanguage(currentLanguage);
+        changeProjectsLanguage(currentLanguage);
     });
 });
