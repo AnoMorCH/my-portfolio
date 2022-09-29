1e547c475fb879769e640bee40bdff78e61d7ada
Anton Morozov <anomorch@gmail.com>
2022-09-29 09:54:36 +0300

    multilanguage of the about-me

--- my-portfolio/static/scripts/language.js
+++ my-portfolio/static/scripts/language.js
@@ -12,6 +12,15 @@ const data = {
             'recognition': 'Recognition',
             'english': 'En',
             'russian': 'Ru'
+        },
+        'about-me': {
+            'name': 'Anton Morozov',
+            'p1': 'Beginner passionate Russian full-stack developer who ' +
+                'can help you realize the project which will show the ' +
+                'World your values.',
+            'p2': 'My target is to deliver to society your idea which ' +
+                'will promote your business and make your work recognizable.',
+            'img': 'My Photo'
         }
     },
     'russian': {
@@ -23,6 +32,15 @@ const data = {
             'recognition': 'Признание',
             'english': 'Англ',
             'russian': 'Рус'
+        },
+        'about-me': {
+            'name': 'Антон Морозов',
+            'p1': 'Начинающий увлеченный российский full-stack ' +
+                'разработчик, готовый помочь вам реализовать проект, ' +
+                'который покажет всему миру ваши настоящие ценности.',
+            'p2': 'Моя цель - это продвижение в общество вашей идеи, ' +
+                'которая продвинет ваше дело и сделает его узнаваемым.',
+            'img': 'Моя Фотография'
         }
     }
 }
@@ -37,7 +55,7 @@ function changeCurrentLanguage(newLanguage) {
 
 function changeNavbarLanguage(currentLanguage) {
     const logo = header.getElementsByClassName('logo')[0];
-    
+
     const burger = header.getElementsByClassName('burger-menu')[0];
     const me = burger.querySelector('#me');
     const projects = burger.querySelector('#projects');
@@ -58,10 +76,28 @@ function changeNavbarLanguage(currentLanguage) {
     russian.textContent = data[currentLanguage]['header']['russian'];
 }
 
+function changeAboutMeLanguage(currentLanguage) {
+    const aboutMe = document.getElementsByClassName('about-me')[0];
+    
+    const img = aboutMe.querySelector('img');
+    
+    const description = aboutMe.getElementsByClassName('description')[0];
+    const name = description.querySelector('h1');
+    const p1 = description.querySelectorAll('p')[0];
+    const p2 = description.querySelectorAll('p')[1];
+
+    img.alt = data[currentLanguage]['about-me']['img'];
+    
+    name.textContent = data[currentLanguage]['about-me']['name'];
+    p1.textContent = data[currentLanguage]['about-me']['p1'];
+    p2.textContent = data[currentLanguage]['about-me']['p2'];
+}
+
 languagesLink.forEach(newCurrentLanaguge => {
     newCurrentLanaguge.addEventListener('click', () => {
         changeCurrentLanguage(newCurrentLanaguge);
         const currentLanguage = newCurrentLanaguge.getAttribute('language');
         changeNavbarLanguage(currentLanguage);
+        changeAboutMeLanguage(currentLanguage);
     });
 });
